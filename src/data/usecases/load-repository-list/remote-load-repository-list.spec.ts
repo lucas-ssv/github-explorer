ee12ba251373afe29cd187e649a7ba32102f231d
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-06 11:12:11 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-repository-list/remote-load-repository-list.spec.ts
+++ src/data/usecases/load-repository-list/remote-load-repository-list.spec.ts
@@ -1,20 +1,31 @@
 import { LoadRepositoryListSpy } from '@/data/usecases'
 import { HttpGetClientSpy } from '@/data/test'
 
+type SutTypes = {
+  sut: LoadRepositoryListSpy
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = 'any_url'): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new LoadRepositoryListSpy(url, httpGetClientSpy)
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('LoadRepositoryList', () => {
   test('Should LoadRepositoryList calls HttpGetClient with correct url', async () => {
     const url = 'any_url'
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new LoadRepositoryListSpy(url, httpGetClientSpy)
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.load('any_repo')
     expect(httpGetClientSpy.url).toBe(url)
   })
 
   test('Should LoadRepositoryList calls HttpGetClient with correct params', async () => {
-    const url = 'any_url'
     const repository = 'any_repo'
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new LoadRepositoryListSpy(url, httpGetClientSpy)
+    const { sut, httpGetClientSpy } = makeSut()
     await sut.load(repository)
     expect(httpGetClientSpy.options).toEqual({ params: { q: repository } })
   })
