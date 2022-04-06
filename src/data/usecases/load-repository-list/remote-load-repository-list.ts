6cd1bb6369650183904036027d31302ac485775c
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-06 14:00:55 -0300

    feat: ensure RemoteLoadRepositoryList returns RepositoryList on status 200

--- src/data/usecases/load-repository-list/remote-load-repository-list.ts
+++ src/data/usecases/load-repository-list/remote-load-repository-list.ts
@@ -1,16 +1,18 @@
+import { RepositoryListModel } from '@/domain/models'
+import { LoadRepositoryList } from '@/domain/usecases'
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
 import { BadRequestError, NotFoundError, ServerError } from '@/domain/errors'
 
-export class LoadRepositoryListSpy {
+export class LoadRepositoryListSpy implements LoadRepositoryList {
   constructor (
     private readonly url: string,
     private readonly httpGetClientSpy: HttpGetClient
   ) {}
 
-  async load (repository: string): Promise<void> {
+  async load (repository: string): Promise<RepositoryListModel> {
     const httpResponse = await this.httpGetClientSpy.get(this.url, { params: { q: repository } })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.notFound: throw new NotFoundError()
       case HttpStatusCode.serverError: throw new ServerError()
       default: throw new BadRequestError()
