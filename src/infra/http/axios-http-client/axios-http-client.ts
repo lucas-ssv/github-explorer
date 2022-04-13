3ba7df5abcf086f7a109fb361c5c893149a77315
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-13 09:56:11 -0300

    feat: ensure AxiosHttpClient returns correct statusCode and body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,12 +1,16 @@
-import { HttpRequest } from '@/data/protocols/http'
+import { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async request (data: HttpRequest): Promise<void> {
-    await axios.request({
+export class AxiosHttpClient implements HttpClient {
+  async request (data: HttpRequest): Promise<HttpResponse> {
+    const axiosResponse = await axios.request({
       url: data.url,
       method: data.method,
       params: data.params
     })
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
   }
 }
