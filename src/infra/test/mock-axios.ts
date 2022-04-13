5f25e4b3165aef6e0af7a89b4b39485397b93e12
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-13 10:01:50 -0300

    refactor: add faker to the static values

--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -1,8 +1,9 @@
+import faker from '@faker-js/faker'
 import axios, { AxiosResponse } from 'axios'
 
 export const mockHttpResponse = (): Omit<AxiosResponse, 'headers' | 'config' | 'statusText'> => ({
-  status: 200,
-  data: 'any_data'
+  status: faker.datatype.number(),
+  data: faker.random.arrayElements()
 })
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
