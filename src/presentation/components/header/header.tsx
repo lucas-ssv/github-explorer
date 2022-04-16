28e07183ed7f9808cca868839d4f4282852126e6
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-16 17:12:41 -0300

    refactor: move header to its own file

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,16 @@
+import Styles from './header-styles.scss'
+import React, { memo } from 'react'
+
+const HeaderComponent: React.FC = () => {
+  return (
+    <header className={Styles.headerWrap}>
+      <img
+        src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0wIDE2QzAgNy4xNjAwMiA3LjE2MDAyIDAgMTYgMEMyNC44NCAwIDMyIDcuMTYwMDIgMzEuOTk5OSAxNkMzMS45OTk5IDI0LjgzMiAyNC44Mzk5IDMxLjk5OTkgMTYgMzEuOTk5OUM3LjE2MDAyIDMxLjk5OTkgMCAyNC44MzIgMCAxNlpNNi4zOTk5NyAyNS42TDE5LjUwNCAxOS41MDRMMjUuNiA2LjM5OTk3TDEyLjQ5NiAxMi40OTZMNi4zOTk5NyAyNS42Wk0xNy43NiAxNkMxNy43NiAxNi45NjggMTYuOTc1OSAxNy43NiAxNiAxNy43NkMxNS4wMzIgMTcuNzYgMTQuMjM5OSAxNi45NjggMTQuMjM5OSAxNkMxNC4yMzk5IDE1LjAzMTkgMTUuMDMxOSAxNC4yMzk5IDE2IDE0LjIzOTlDMTYuOTc2IDE0LjIzOTkgMTcuNzYgMTUuMDMxOSAxNy43NiAxNloiIGZpbGw9IiMxMjEyMTQiLz4KPC9zdmc+Cg=="
+        alt="Github Explorer"
+      />
+      <strong>github<span>_explorer</span></strong>
+    </header>
+  )
+}
+
+export const Header = memo(HeaderComponent)
