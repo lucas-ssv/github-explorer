a136a2cfa7c3d745391103e108ca2189c19a0453
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-22 14:50:57 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/repository/item/item.spec.tsx
+++ src/presentation/components/repository/item/item.spec.tsx
@@ -2,10 +2,14 @@ import { RepositoryItem } from './item'
 import { render, screen } from '@testing-library/react'
 import { mockRepositoryList } from '@/data/test'
 
+const makeSut = (repositoryListMock = mockRepositoryList()): void => {
+  render(<RepositoryItem repository={repositoryListMock} />)
+}
+
 describe('RepositoryItem', () => {
   test('Should render with correct values', () => {
     const repositoryListMock = mockRepositoryList()
-    render(<RepositoryItem repository={repositoryListMock} />)
+    makeSut(repositoryListMock)
     expect(screen.getByTestId('image-profile')).toHaveProperty('src', repositoryListMock.owner.avatar_url)
     expect(screen.getByTestId('image-profile')).toHaveProperty('alt', repositoryListMock.owner.name)
     expect(screen.getByTestId('full-name')).toHaveTextContent(repositoryListMock.fullName)
