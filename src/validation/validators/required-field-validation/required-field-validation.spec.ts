0ff9b9902e54c060295aa84141975eb554dba193
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-22 16:50:47 -0300

    fix: change type of param

--- src/validation/validators/required-field-validation/required-field-validation.spec.ts
+++ src/validation/validators/required-field-validation/required-field-validation.spec.ts
@@ -4,13 +4,13 @@ import { RequiredFieldError } from '@/validation/errors'
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('any_field')
-    const error = sut.validate('')
+    const error = sut.validate({ any_field: '' })
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('Should return falsy if field is not empty', () => {
     const sut = new RequiredFieldValidation('any_field')
-    const error = sut.validate('any_value')
+    const error = sut.validate({ any_field: 'any_value' })
     expect(error).toBeFalsy()
   })
 })
