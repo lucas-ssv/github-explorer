b43353389b04183a51bd59b8c414235409998c28
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-22 10:33:27 -0300

    test: ensure Home renders RepositoryList on success

--- src/data/test/mock-repository-list.ts
+++ src/data/test/mock-repository-list.ts
@@ -14,5 +14,17 @@ export const mockRepositoryList = (): RepositoryListModel => ({
     owner: {
       name: faker.name.firstName()
     }
+  }, {
+    id: faker.datatype.number(),
+    name: faker.random.word(),
+    fullName: faker.name.findName(),
+    description: faker.random.words(),
+    htmlUrl: faker.internet.url(),
+    starsCount: faker.datatype.number(),
+    forksCount: faker.datatype.number(),
+    issuesCount: faker.datatype.number(),
+    owner: {
+      name: faker.name.firstName()
+    }
   }]
 })
--- src/presentation/pages/home/home.spec.tsx
+++ src/presentation/pages/home/home.spec.tsx
@@ -2,7 +2,7 @@ import { Home } from '@/presentation/pages'
 import { LoadRepositoryList } from '@/domain/usecases'
 import { RepositoryListModel } from '@/domain/models'
 import { mockRepositoryList } from '@/data/test'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { faker } from '@faker-js/faker'
 
 type SutTypes = {
@@ -27,7 +27,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('HomePage', () => {
-  test('Should start with initial state correct', () => {
+  test('Should start with initial state correct', async () => {
     makeSut()
     const loadingWrap = screen.queryByTestId('loading-wrap')
     const error = screen.queryByTestId('error')
@@ -41,7 +41,7 @@ describe('HomePage', () => {
     expect(submitButton).toBeDisabled()
   })
 
-  test('Should enable submit button if form is valid', () => {
+  test('Should enable submit button if form is valid', async () => {
     makeSut()
     const input = screen.getByTestId('repository-input')
     fireEvent.input(input, { target: { value: faker.random.word() } })
@@ -49,32 +49,46 @@ describe('HomePage', () => {
     expect(submitButton).toBeEnabled()
   })
 
-  test('Should show loading if form is valid', () => {
+  test('Should show loading if form is valid', async () => {
     makeSut()
     const input = screen.getByTestId('repository-input')
     fireEvent.input(input, { target: { value: faker.random.word() } })
     const submitButton = screen.getByTestId('submit-button')
     fireEvent.click(submitButton)
+    await waitFor(() => screen.getByTestId('repositories-wrap'))
     const loadingWrap = screen.queryByTestId('loading-wrap')
     expect(loadingWrap).toBeInTheDocument()
   })
 
-  test('Should call LoadRepositoryList on submit button', () => {
+  test('Should call LoadRepositoryList on submit button', async () => {
     const { loadRepositoryListSpy } = makeSut()
     const input = screen.getByTestId('repository-input')
     fireEvent.input(input, { target: { value: faker.random.word() } })
     const submitButton = screen.getByTestId('submit-button')
     fireEvent.click(submitButton)
+    await waitFor(() => screen.getByTestId('repositories-wrap'))
     expect(loadRepositoryListSpy.callsCount).toBe(1)
   })
 
-  test('Should call LoadRepositoryList only once', () => {
+  test('Should call LoadRepositoryList only once', async () => {
     const { loadRepositoryListSpy } = makeSut()
     const input = screen.getByTestId('repository-input')
     fireEvent.input(input, { target: { value: faker.random.word() } })
     const submitButton = screen.getByTestId('submit-button')
     fireEvent.click(submitButton)
     fireEvent.click(submitButton)
+    await waitFor(() => screen.getByTestId('repositories-wrap'))
     expect(loadRepositoryListSpy.callsCount).toBe(1)
+  })
+
+  test('Should render RepositoryList on success', async () => {
+    makeSut()
+    const input = screen.getByTestId('repository-input')
+    fireEvent.input(input, { target: { value: faker.random.word() } })
+    const submitButton = screen.getByTestId('submit-button')
+    fireEvent.click(submitButton)
+    await waitFor(() => screen.getByTestId('repositories-wrap'))
+    const repositoryItem = screen.getAllByTestId('repository-item')
+    expect(repositoryItem).toHaveLength(2)
   })
 })
