d41129fe1cc276d545104c022570f30f8288256f
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-25 09:32:26 -0300

    feat: ensure ValidationBuilder has a required helper

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,2 +1,3 @@
 export * from './required-field-validation/required-field-validation'
 export * from './validation-composite/validation-composite'
+export * from './validation-builder/validation-builder'
