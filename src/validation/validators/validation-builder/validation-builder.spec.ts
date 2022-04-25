19ed6e82e1bd4c9f6c800f8ae957e094f56bebff
lucas-ssv <lucas_99silva@hotmail.com>
2022-04-25 09:56:20 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-builder/validation-builder.spec.ts
+++ src/validation/validators/validation-builder/validation-builder.spec.ts
@@ -1,9 +1,13 @@
 import { RequiredFieldValidation, ValidationBuilder } from '@/validation/validators'
+import { faker } from '@faker-js/faker'
+
+const makeSut = (field: string): ValidationBuilder => ValidationBuilder.field(field)
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const sut = ValidationBuilder.field('any_field')
+    const field = faker.database.column()
+    const sut = makeSut(field)
     const validations = sut.required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 })
