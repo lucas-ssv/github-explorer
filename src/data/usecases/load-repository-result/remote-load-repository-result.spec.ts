e2adba789d9dd6b06e9179c92f1fa79f811e0e75
Lucas <lucas_99silva@hotmail.com>
2022-04-30 18:30:43 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-repository-result/remote-load-repository-result.spec.ts
+++ src/data/usecases/load-repository-result/remote-load-repository-result.spec.ts
@@ -1,20 +1,33 @@
 import { LoadRepositoryResult } from '@/data/usecases'
-import { HttpClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
+import { HttpClientSpy } from '@/data/test'
 import { BadRequestError } from '@/domain/errors'
+import { faker } from '@faker-js/faker'
+
+type SutTypes = {
+  sut: LoadRepositoryResult
+  httpClientSpy: HttpClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy()
+  const sut = new LoadRepositoryResult(url, httpClientSpy)
+  return {
+    sut,
+    httpClientSpy
+  }
+}
 
 describe('LoadRepositoryResult', () => {
   test('Should LoadRepositoryResult calls HttpClient with correct url', async () => {
-    const url = 'any_url'
-    const httpClientSpy = new HttpClientSpy()
-    const sut = new LoadRepositoryResult(url, httpClientSpy)
+    const url = faker.internet.url()
+    const { sut, httpClientSpy } = makeSut(url)
     await sut.load()
     expect(httpClientSpy.url).toBe(url)
   })
 
   test('Should LoadRepositoryResult returns BadRequestError on status 400', async () => {
-    const httpClientSpy = new HttpClientSpy()
-    const sut = new LoadRepositoryResult('any_url', httpClientSpy)
+    const { sut, httpClientSpy } = makeSut()
     httpClientSpy.response = {
       statusCode: HttpStatusCode.badRequest
     }
