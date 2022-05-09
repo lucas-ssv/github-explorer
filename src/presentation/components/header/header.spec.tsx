f3f9377da72e5750436d428a5de8cc4693a70b12
Lucas <lucas_99silva@hotmail.com>
2022-05-09 15:22:19 -0300

    test: ensure Header shows back button if has param id in the url

---
+++ src/presentation/components/header/header.spec.tsx
@@ -0,0 +1,24 @@
+import { Header } from '@/presentation/components'
+import Router, { Router as RouterNavigate } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import { createMemoryHistory } from 'history'
+import { faker } from '@faker-js/faker'
+
+jest.mock('react-router-dom', (): any => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn()
+}))
+
+const history = createMemoryHistory()
+
+describe('HeaderComponent', () => {
+  test('Should show back button if has param id in the url', () => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ id: faker.random.word() })
+    render(
+      <RouterNavigate location={history.location} navigator={history}>
+        <Header />
+      </RouterNavigate>
+    )
+    expect(screen.queryByTestId('back-link')).toBeInTheDocument()
+  })
+})
