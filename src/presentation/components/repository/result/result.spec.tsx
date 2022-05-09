d24f652d7c6007130451191635e0e44edb31c904
Lucas <lucas_99silva@hotmail.com>
2022-05-09 18:06:53 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/repository/result/result.spec.tsx
+++ src/presentation/components/repository/result/result.spec.tsx
@@ -1,30 +1,38 @@
+import { RepositoryResult } from '@/domain/models'
 import { mockRepositoryResult } from '@/data/test'
 import { RepositoryResultItem } from '@/presentation/components'
 import { fireEvent, render, screen } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 
-const history = createMemoryHistory({ initialEntries: ['/:id'] })
+type SutTypes = {
+  repositoryResultMock: RepositoryResult
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/:id'] })
+  const repositoryResultMock = mockRepositoryResult()
+  render(
+    <Router location={history.location} navigator={history}>
+      <RepositoryResultItem repository={repositoryResultMock} />
+    </Router>
+  )
+  return {
+    repositoryResultMock,
+    history
+  }
+}
 
 describe('RepositoryResultItem', () => {
   test('Should render with correct values', () => {
-    const repositoryResultMock = mockRepositoryResult()
-    render(
-      <Router location={history.location} navigator={history}>
-        <RepositoryResultItem repository={repositoryResultMock} />
-      </Router>
-    )
+    const { repositoryResultMock } = makeSut()
     expect(screen.getByTestId('repository-full-name')).toHaveTextContent(repositoryResultMock.fullName)
     expect(screen.getByTestId('repository-description-result')).toHaveTextContent(repositoryResultMock.description)
   })
 
   test('Should open repository github on click', () => {
-    const repositoryResultMock = mockRepositoryResult()
-    render(
-      <Router location={history.location} navigator={history}>
-        <RepositoryResultItem repository={repositoryResultMock} />
-      </Router>
-    )
+    const { repositoryResultMock, history } = makeSut()
     const linkHtmlUrl = screen.getByTestId('html-url')
     fireEvent.click(linkHtmlUrl)
     expect(history.location.pathname).toBe(`/${repositoryResultMock.htmlUrl}`)
