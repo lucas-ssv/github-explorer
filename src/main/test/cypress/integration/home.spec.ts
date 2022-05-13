313e9a5be6533dd7b675da88eb3beaa75ae9b154
Lucas <lucas_99silva@hotmail.com>
2022-05-13 14:17:21 -0300

    test: ensure Home loads with correct initial state

--- src/main/test/cypress/integration/home.spec.ts
+++ src/main/test/cypress/integration/home.spec.ts
@@ -1,5 +1,11 @@
 describe('Home', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('')
+  })
+
+  it('Should load Home with correct initial state', () => {
+    cy.getByTestId('submit-button').should('be.disabled')
+    cy.getByTestId('loading-wrap').should('not.exist')
+    cy.getByTestId('error').should('not.exist')
   })
 })
