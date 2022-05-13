1be6fa19342a26ce6f47064be55e244c223dd2c1
Lucas <lucas_99silva@hotmail.com>
2022-05-13 16:12:40 -0300

    test: ensure Home not present loading on finish request

--- src/main/test/cypress/integration/home.spec.ts
+++ src/main/test/cypress/integration/home.spec.ts
@@ -21,4 +21,13 @@ describe('Home', () => {
     cy.getByTestId('submit-button').click()
     cy.getByTestId('loading-wrap').should('exist')
   })
+
+  it('Should not present loading on finish request', () => {
+    cy.getByTestId('repository-input').type(faker.random.word())
+    cy.getByTestId('submit-button').click()
+      .getByTestId('loading-wrap').should('exist')
+      .getByTestId('text-search-button').should('not.exist')
+      .getByTestId('loading-wrap').should('not.exist')
+      .getByTestId('text-search-button').should('exist')
+  })
 })
